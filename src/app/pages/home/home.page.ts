6bdc67e9b0c82a7e1ae0ba23d620540622454598
nipimenta <nicolasaraujopimenta05@gmail.com>
2026-03-03 18:26:55 -0300

    primeira versão carrossel

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -2,12 +2,13 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { SobreComponent } from '../../components/sobre/sobre.component';
 import { ProjetosComponent } from '../../components/projetos/projetos.component';
+import { ExperienciaComponent } from '../../components/experiencia/experiencia.components';
 import { invokeParticles } from '../../app';
 
 @Component({
   styleUrl: './home.scss',
   templateUrl: './home.html',
-  imports: [CommonModule, SobreComponent, ProjetosComponent],
+  imports: [CommonModule, SobreComponent, ProjetosComponent, ExperienciaComponent],
 })
 export class HomePage {
   ngOnInit(): void {
